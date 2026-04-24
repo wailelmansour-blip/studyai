6fe1360e3734e1b5a5387ffc745acfb593ba6e80
elmansour <wail.elmansour@gmail.com>
2026-04-24 12:07:05 +0200

    erreur dans index

--- app/index.tsx
+++ app/index.tsx
@@ -4,10 +4,10 @@ export default function Page() {
       return (
               <View style={styles.container}>
                         <View style={styles.main}>
-                                    <Text style={styles.title}>Hello World test</Text>Text>
-                                    <Text style={styles.subtitle}>This is the first page of your app.</Text>Text>
-                        </View>View>
-              </View>View>
+                                    <Text style={styles.title}>Hello World test</Text>
+                                    <Text style={styles.subtitle}>This is the first page of your app.</Text>
+                        </View>
+              </View>
             );
 }
 
